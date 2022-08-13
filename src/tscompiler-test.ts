b14cd6342ecb87664ee03ac91735b06807602a9f
Paul Davies <lagdotcom@gmail.com>
2022-08-13 13:41:44 +0100

    just realised my rlsrc directory is all wrong

--- src/tscompiler-test.ts
+++ src/tscompiler-test.ts
@@ -6,7 +6,7 @@ import grammar from "./grammar";
 import { readFileSync } from "fs";
 
 const parser = new Parser(grammar as unknown as Grammar);
-const src = readFileSync("./rlsrc/week6.rlscript", { encoding: "utf-8" });
+const src = readFileSync("./rlsrc/part8.rlscript", { encoding: "utf-8" });
 parser.feed(src);
 
 if (parser.results.length === 0) console.error("No possible parsings.");
